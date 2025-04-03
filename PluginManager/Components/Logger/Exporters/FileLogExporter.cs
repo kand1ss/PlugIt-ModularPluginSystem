8b300e6e63e395763592f9cbac6006caae013c44
kand1ss <145040374+kand1ss@users.noreply.github.com>
2025-04-03 13:55:07 +0300

    FileLogExporter Improvement

--- PluginManager/Components/Logger/Exporters/FileLogExporter.cs
+++ PluginManager/Components/Logger/Exporters/FileLogExporter.cs
@@ -2,13 +2,50 @@ using ModularPluginAPI.Components.Logger.Interfaces;
 
 namespace ModularPluginAPI.Components.Logger.Exporters;
 
-public class FileLogExporter(string path) : ILogExporter
+/// <summary>
+/// A class responsible for exporting log messages to a file.
+/// </summary>
+/// <remarks>
+/// If the file name is not provided, a default name is generated based on the current date and time.
+/// If a file with the same name already exists, a numeric suffix (e.g., "(1)", "(2)", etc.) is added.
+/// </remarks>
+/// <param name="path">The directory where the log file will be saved.</param>
+/// <param name="fileName">Optional. The name of the log file. If null, a default name is used.</param>
+public class FileLogExporter(string path, string? fileName = null) : ILogExporter
 {
+    private string? _fileName = fileName;
+
+    /// <summary>
+    /// Exports log messages to a file.
+    /// </summary>
+    /// <param name="messages">A collection of log messages to be written to the file.</param>
+    /// <remarks>
+    /// If the file name is not specified, it is automatically generated in the format "Log_yyyy-MM-dd_HH-mm-ss.log".
+    /// The method ensures that the file has a ".log" extension.
+    /// If a file with the same name already exists, a numeric suffix (e.g., "(1)", "(2)", etc.) is added to create a unique file name.
+    /// </remarks>
     public void Export(IEnumerable<string> messages)
     {
-        var date = DateTime.Now;
-        var fileName = $"Log_{date:yyyy-MM-dd_HH-mm-ss}.log";
-        var fullPath = Path.Combine(path, fileName);
+        if (_fileName is null)
+        {
+            var date = DateTime.Now;
+            _fileName = $"Log_{date:yyyy-MM-dd_HH-mm-ss}.log";
+        }
+        if (!Path.HasExtension(_fileName))
+            _fileName += ".log";
+        
+        var fullPath = Path.Combine(path, _fileName);
+        int counter = 1;
+        
+        while (File.Exists(fullPath))
+        {
+            var fileExtension = Path.GetExtension(fullPath);
+            var newFileName = Path.GetFileNameWithoutExtension(_fileName);
+            newFileName += $" ({counter})" + fileExtension;
+            
+            fullPath = Path.Combine(path, newFileName);
+            counter++;
+        }
         
         File.AppendAllLines(fullPath, messages);
     }
