3b5fbb33054f2b11713cc9b563e0a55ed989df10
kand1ss <145040374+kand1ss@users.noreply.github.com>
2025-04-06 15:42:24 +0300

    Complete restructuring of the project

---
+++ tests/Plugins/FaultedPlugin.cs
@@ -0,0 +1,17 @@
+using PluginAPI;
+
+namespace Plugins;
+
+public class FaultedPlugin : PluginBase, IExecutablePlugin
+{
+    public override string Name => "FaultedPlugin";
+    public override Version Version => new(1, 0, 0);
+    public override string Description => "Faulted plugin";
+    public override string Author => "kand1ss";
+    
+    
+    public void Execute()
+    {
+        throw new Exception("Faulted plugin");
+    }
+}
