1ea81640c3b178ed11379fa49c69b7de30c82c7d
kand1ss <145040374+kand1ss@users.noreply.github.com>
2025-04-17 22:50:06 +0300

    Vulnerability that allows creating a fake secure service has been resolved

--- src/PluginManager/Services/Security Service/AssemblySecurityService.cs
+++ src/PluginManager/Services/Security Service/AssemblySecurityService.cs
@@ -1,6 +1,10 @@
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Security;
 using System.Text.RegularExpressions;
 using ModularPluginAPI.Services.Interfaces;
 using Mono.Cecil;
+using PluginAPI.Services.interfaces;
 
 namespace ModularPluginAPI.Components;
 
@@ -12,7 +16,10 @@ public class AssemblySecurityService : IAssemblySecurityService
         "System.Reflection.Emit",
         "System.Net"
     };
-
+    
+    private readonly HashSet<string> _checkedAssemblies = new();
+    
+    
     public bool AddBlockedNamespace(string namespaceName)
     {
         var regularExpression = new Regex(@"^([A-Za-z]+)(\.([A-Za-z]+))*$", RegexOptions.Compiled);
@@ -25,10 +32,43 @@ public class AssemblySecurityService : IAssemblySecurityService
     public bool RemoveBlockedNamespace(string namespaceName)
         => _blockedNamespaces.Remove(namespaceName);
 
+
     public bool CheckSafety(string assemblyPath)
     {
+        if (!_checkedAssemblies.Add(assemblyPath))
+            return true;
+
         var assembly = AssemblyDefinition.ReadAssembly(assemblyPath);
+        var resolver = new AssemblyDependencyResolver(assemblyPath);
+        bool result = AnalyzeNamespaces(assembly) && AnalyzeInterfaces(assembly);
         
+        foreach (var reference in assembly.MainModule.AssemblyReferences)
+        {
+            if (IsTrustedAssembly(reference.Name))
+                continue;
+
+            var resolvedPath = resolver.ResolveAssemblyToPath(new AssemblyName(reference.FullName));
+            if (resolvedPath != null)
+                result &= CheckSafety(resolvedPath);
+        }
+
+        return result;
+    }
+    
+    private bool CheckSafety(AssemblyDefinition assembly)
+        => AnalyzeNamespaces(assembly) && AnalyzeInterfaces(assembly);
+
+    private bool IsTrustedAssembly(string name)
+    {
+        return name.StartsWith("System.", StringComparison.OrdinalIgnoreCase) ||
+               name.StartsWith("Microsoft.", StringComparison.OrdinalIgnoreCase) ||
+               name.Equals("PluginAPI", StringComparison.OrdinalIgnoreCase) ||
+               name == "System" || name == "mscorlib" || name == "netstandard";
+    }
+
+
+    private bool AnalyzeNamespaces(AssemblyDefinition assembly)
+    {
         foreach (var module in assembly.Modules)
         {
             foreach (var type in module.Types)
@@ -48,6 +88,22 @@ public class AssemblySecurityService : IAssemblySecurityService
                         }
                     }
                 }
+            }
+        }
+
+        return true;
+    }
+
+    private bool AnalyzeInterfaces(AssemblyDefinition assembly)
+    {
+        foreach (var type in assembly.MainModule.Types)
+        {
+            foreach (var @interface in type.Interfaces)
+            {
+                if (@interface.InterfaceType.FullName == typeof(IPluginFileSystemService).FullName)
+                    throw new SecurityException($"A fake secure file system service has been detected in the assembly: '{assembly.Name}'");
+                if (@interface.InterfaceType.FullName == typeof(IPluginNetworkService).FullName)
+                    throw new SecurityException($"A fake secure network service has been detected in the assembly: '{assembly.Name}'");
             }
         }
 
